d68d796e1364880f7b8a262415c99b6e33af6e79
sigenogoro <25719873+sigenogoro@users.noreply.github.com>
2025-07-31 01:00:02 +0900

    v-model追加

--- src/components/step2.vue
+++ src/components/step2.vue
@@ -1,18 +1,25 @@
 <template>
     <div>
-        <h1>Vue3学習 - Step2</h1>
+        <h1>Vue3学習 - Step2 & 3 reactive, v-model</h1>
         <p>名前: {{ userObject.name }}</p>
         <p>年齢: {{ userObject.age }}</p>
+
+        <!-- Step3: v-model -->
+         <label>メッセージの入力: </label>
+         <input v-model="message" type="text">
+         <p>入力内容: {{ message }}</p>
     </div>
 </template>
 
 <script setup lang="ts">
-import {reactive} from 'vue'
+import {ref, reactive} from 'vue'
 
 const userObject = reactive({
     name: "Vue太郎",
     age: 25
 })
+
+const message = ref<string>("")
 
 
 </script>
